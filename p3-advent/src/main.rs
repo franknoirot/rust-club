d8c1681b71ba92d2e35475787c69652722e3979d
Frank Johnson <frankjohnson1993@gmail.com>
2023-07-17 14:22:29 -0400

    WIP for part 2

--- p3-advent/src/main.rs
+++ p3-advent/src/main.rs
@@ -1,6 +1,16 @@
 fn main() {
+    // Answer 1
+    let checksum = get_checksum();
+    assert_eq!(checksum, 51833); 
+    println!("Answer 1 is {}", checksum);
+
+    let divisible_checksum = get_divisible_checksum();
+    println!("Answer 2 is {}", divisible_checksum);
+}
+
+fn load_parse_tsv(path: &str) -> Vec<Vec<i32>> {
     // Load in the input TSV file
-    let raw_input = std::fs::read_to_string("input.txt")
+    let raw_input = std::fs::read_to_string(path)
         .unwrap();
 
     // Parse the input file (TSV) into a 2D vector of i32s
@@ -11,14 +21,33 @@ fn main() {
         .map(|line| line.collect())
         .collect();
 
+    return parsed_input
+}
+
+// Function for the first part of the problem
+fn get_checksum() -> i32 {
+    let parsed_input = load_parse_tsv("input.txt");
+
     // Get the difference between the min and max values in each line,
     // then get the sum of those differences
     let differences = parsed_input.iter()
         .map(|line| line.iter().max().unwrap() - line.iter().min().unwrap());
 
-    // Sum the differences
-    let sum : i32 = differences.sum();
+    // Return the sum of the differences
+    differences.sum()
+}
 
-    // Print the sum
-    println!("{}", sum);
+// Function for the second part of the problem
+fn get_divisible_checksum() -> i32 {
+    let parsed_input = load_parse_tsv("input.txt");
+
+    // Sort the items in each row, smallest to largest
+    // Then see if each item in the row divides evenly with any others
+    // TODO: I don't know how to sort each line and pass that through
+    //  the map() functions I want to use
+    let divided_lines = parsed_input.iter()
+        .for_each(|&mut line| line.sort());
+
+    // dummy output for now
+    return 1
 }
